7f360aa89aa48be8cc4b5fa46f30e073bf5a2fb4
Ushnika09 <karushnika@gmail.com>
2026-03-28 20:57:52 +0530

    BE updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -202,3 +202,14 @@ export const selectCharity = async (req, res) => {
     });
   }
 };
+
+export const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    res.json({ user });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addScore } from "../controllers/userController.js";
+import { addScore, getProfile } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { subscribeUser } from "../controllers/userController.js";
 import { selectCharity } from "../controllers/userController.js";
@@ -10,5 +10,6 @@ const router = express.Router();
 router.post("/add-score", protect, addScore);
 router.post("/subscribe", protect, subscribeUser);
 router.post("/select-charity", protect, selectCharity);
+router.get("/profile", protect, getProfile);
 
 export default router;
